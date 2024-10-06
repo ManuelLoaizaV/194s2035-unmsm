cf9ceea47ba7aa0f2b3b668014bbc97bb20ff411
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-10-06 13:58:03 -0500

    Add management problem

---
+++ ejercicios/04_clases/gestion/main.py
@@ -0,0 +1,9 @@
+class Empleado:
+    # Variables de clase
+
+    # Métodos
+    def __init__(self, primer_nombre, apellido, dni, puesto, salario):
+        pass
+
+    def obtener_nombre(self):
+        pass
