b9d86973b73a69a063d9a7602ea4155765b6e14b
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-11-23 22:05:29 -0500

    list vs tuple

---
+++ clases/09_funcional/tuplas.py
@@ -0,0 +1,12 @@
+print("Orientado a objetos:")
+notas_oop = [5, 9, 3, 2, 10]
+print(notas_oop)
+notas_oop.append(19)
+print(notas_oop)
+
+print("Funcional")
+notas_fp = (5, 9, 3, 2, 10)
+print(notas_fp)
+nuevas_notas = notas_fp + (19,)
+print(notas_fp)
+print(nuevas_notas)
