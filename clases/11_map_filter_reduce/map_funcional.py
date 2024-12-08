4510322740f73a8ce046e31e0cfc293ab2ebc5be
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-12-07 21:46:11 -0500

    add map examples

---
+++ clases/11_map_filter_reduce/map_funcional.py
@@ -0,0 +1,9 @@
+import math
+
+numeros = [1, 4, 9, 16]
+raices = list(map(math.sqrt, numeros))
+print(raices)
+
+nombres = ["MaNuEl", "Bryan", "daira", "JAVIER"]
+nombres_en_minusculas = list(map(str.lower, nombres))
+print(nombres_en_minusculas)
---
+++ clases/11_map_filter_reduce/map_imperativa.py
@@ -0,0 +1,14 @@
+import math
+
+numeros = [1, 4, 9, 16]
+raices = []
+for numero in numeros:
+    raices.append(math.sqrt(numero))
+print(raices)
+
+nombres = ["MaNuEl", "Bryan", "daira", "JAVIER"]
+nombres_en_minusculas = []
+for nombre in nombres:
+    nombre_en_minusculas = nombre.lower()
+    nombres_en_minusculas.append(nombre_en_minusculas)
+print(nombres_en_minusculas)
